9a988253d78dd175a3947bf195049d696fa550d3
Benjamin DeVore <bdevore@123devore.net>
2015-01-22 22:01:29 -0500

    Added comments and implemented methods for assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions for performing matrix investion. Includes a caching matrix object that can
+## be used for increasing performance for the expensive matrix inversion operation
 
-## Write a short comment describing this function
-
+## Creates special matrix object that is able to cache its inverse for 
+## improved performance
 makeCacheMatrix <- function(x = matrix()) {
-
+  invmat <- NULL
+  set <- function(y) {
+    x <<- y
+    invmat <<- NULL
+  }
+  
+  get <- function() x
+  setinverse <- function(inverse) invmat <<- inverse
+  getinverse <- function() invmat
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Return the inverse of the supplied matrix, using the cached matrix object
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getinverse()
+  if(!is.null(inverse)) {
+    message("Getting cached data")
+    return(inverse)
+  } 
+  data <- x$get()
+  inverse <- solve(data, ...)
+  x$setinverse(inverse)
+  inverse
 }
